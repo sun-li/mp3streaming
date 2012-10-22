51fdb1644725d673444ab4d1501b7d3d58f79b58
Li SUN <SunLi@MrSunLi.com>
2012-10-22 22:21:33 +0800

    play

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,3 +13,16 @@
 //= require jquery
 //= require jquery_ujs
 //= require_tree .
+
+    $(document).ready(function() {  
+        var audioSection = $('section#audio');  
+        $('a.html5').click(function() {  
+            var audio = $('<audio>', {  
+                 controls : 'controls'  
+            });  
+            var url = $(this).attr('href');  
+            $('<source>').attr('src', url).appendTo(audio);  
+            audioSection.html(audio);  
+            return false;  
+        });  
+    });
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,7 @@
 module ApplicationHelper
   BUCKET = 'sunli.streaming'
+
+  def download_url_for(song_key)
+    AWS::S3::S3Object.url_for(song_key, BUCKET, :authenticated => false)
+  end
 end
